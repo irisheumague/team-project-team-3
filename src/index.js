a98f070f7741b4918e3d5575e604ccda271d35c0
Imeh24 <medinajeremymarcel@gmail.com>
2023-09-05 20:56:19 +0800

    1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
+// import './js/swiper.js';
 import './js/mobile-menu.js';
 import './js/buy-now-modal.js';
 import './js/form-buy-now.js';
-import './js/review-swiper.js';
-import './js/swiper.js';
+// import './js/review-swiper.js';
+import './js/subscribe.js';
